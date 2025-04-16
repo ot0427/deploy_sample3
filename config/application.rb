f843bd5520c7a6a9ba268f07319675bba19787b2
ot0427 <s4ww58nea@gmail.com>
2025-04-16 15:27:01 +0900

    ユーザー登録とログイン機能の実装（日本語化対応）

--- config/application.rb
+++ config/application.rb
@@ -17,11 +17,12 @@ module Myapp
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.i18n.default_locale = :ja
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.timestamped_migrations = false
   end
 end
